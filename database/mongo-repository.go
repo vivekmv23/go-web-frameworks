fed072b5487f5d01967179c4476500cfdb1f0099
Vivek Vadakeyil <vivekmv95@gmail.com>
2024-08-31 19:02:40 +0530

    [db]: Adding update method

--- database/mongo-repository.go
+++ database/mongo-repository.go
@@ -2,10 +2,12 @@ package database
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/vivekmv23/go-web-frameworks/lib"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -28,7 +30,7 @@ func getMongoCollection() *mongo.Collection {
 	return client.Database(ITEM_DB).Collection(ITEM_COLLECTION)
 }
 
-func Save(i *Item) error {
+func SaveItem(i *lib.Item) error {
 	mc := getMongoCollection()
 	updateTimes(i)
 	_, err := mc.InsertOne(context.TODO(), i)
@@ -37,22 +39,22 @@ func Save(i *Item) error {
 
 }
 
-func GetById(id uuid.UUID) (Item, error) {
+func GetItemById(id uuid.UUID) (lib.Item, error) {
 	mc := getMongoCollection()
-	var i Item
+	var i lib.Item
 	err := mc.FindOne(context.TODO(), bson.D{{Key: "id", Value: id}}).Decode(&i)
 	return i, mapDbError(err, id)
 
 }
 
-func GetAll() ([]Item, error) {
-	
+func GetAllItems() ([]lib.Item, error) {
+
 	mc := getMongoCollection()
-	
-	var i []Item
-	
+
+	var i []lib.Item
+
 	cur, err := mc.Find(context.TODO(), bson.D{{}})
-	
+
 	if err != nil {
 		return i, mapDbError(err)
 	}
@@ -63,7 +65,7 @@ func GetAll() ([]Item, error) {
 
 }
 
-func DeleteById(id uuid.UUID) error {
+func DeleteItemById(id uuid.UUID) error {
 	mc := getMongoCollection()
 
 	res, err := mc.DeleteOne(context.TODO(), bson.D{{Key: "id", Value: id}})
@@ -72,6 +74,45 @@ func DeleteById(id uuid.UUID) error {
 	}
 
 	return mapDbError(err, id)
+}
+
+func UpdateItem(i lib.Item, ifMatch string) (lib.Item, error) {
+	var existingItem lib.Item
+
+	existingItem, err := GetItemById(i.Id)
+
+	if err != nil {
+		return i, err
+	}
+
+	if existingItem.UpdatedOn.String() != ifMatch {
+		return i, fmt.Errorf("Update request is outdated: if-match(%s) != %s", ifMatch, existingItem.UpdatedOn.String())
+	}
+
+	i.DbId = existingItem.DbId
+	i.CreatedOn = existingItem.CreatedOn
+	updateTimes(&i)
+
+	iDoc, err := toDoc(i)
+	if err != nil {
+		return i, mapDbError(err)
+	}
+
+	mc := getMongoCollection()
+
+	filter := bson.D{{Key: "_id", Value: i.DbId}}
+	update := bson.D{{Key: "$set", Value: iDoc}}
+
+	res, err := mc.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		return i, mapDbError(err)
+	}
+
+	if res.ModifiedCount == 1 {
+		return i, nil
+	}
+
+	return i, fmt.Errorf("failed to update, updated count != 1")
 }
 
 func mapDbError(err error, arg ...any) error {
@@ -91,10 +132,19 @@ func mapDbError(err error, arg ...any) error {
 	return &Unclassified{err: err}
 }
 
-func updateTimes(i *Item) {
+func updateTimes(i *lib.Item) {
 	now := time.Now()
 	if i.CreatedOn.IsZero() {
 		i.CreatedOn = now
 	}
 	i.UpdatedOn = now
+}
+
+func toDoc(v interface{}) (doc *bson.D, err error) {
+	data, err := bson.Marshal(v)
+	if err != nil {
+		return
+	}
+	err = bson.Unmarshal(data, &doc)
+	return
 }
---
+++ lib/structures.go
@@ -0,0 +1,24 @@
+package lib
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Item struct {
+	DbId        primitive.ObjectID `bson:"_id,omitempty"`
+	Id          uuid.UUID          `bson:"id",omitempty json:"id"`
+	Name        string             `bson:"nam",omitempty json:"name"`
+	Value       int                `bson:"val",omitempty json:"value"`
+	Description string             `bson:"dsc",omitempty json:"description"`
+	Active      bool               `bson:"act",omitempty json:"isActive"`
+	CreatedOn   time.Time          `bson:"con",omitempty json:"createdOn"`
+	UpdatedOn   time.Time          `bson:"uon",omitempty json:"updatedOn"`
+}
+
+type Error struct {
+	Error string `json:"error"`
+	Path  string `json:"path"`
+}
